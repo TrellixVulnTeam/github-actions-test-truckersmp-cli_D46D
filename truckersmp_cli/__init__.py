afb80b29987676bc7a96f0ab41299fd1bdb34c4f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 07:49:42 +0000

    Adding tarfile member sanitization to extractall()

--- truckersmp_cli/__init__.py
+++ truckersmp_cli/__init__.py
@@ -629,7 +629,26 @@ def update_game():
         except Exception as e:
             sys.exit("Failed to download steamcmd: {}".format(e))
         with tarfile.open(fileobj=io.BytesIO(steamcmd_targz), mode="r:gz") as f:
-            f.extractall(Dir.steamcmddir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, Dir.steamcmddir)
     logging.info("Steamcmd: " + steamcmd)
 
     # download/update Proton
